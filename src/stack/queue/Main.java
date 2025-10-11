b1ec300938fd42e2f568c86f054df6f11c4d04eb
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-10-11 09:25:30 +0530

    queue

---
+++ src/stack/queue/Main.java
@@ -0,0 +1,19 @@
+package stack.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue q = new Queue(10);
+
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+        q.enqueue(40);
+        q.enqueue(50);
+
+        System.out.println(q);
+        System.out.println(q.dequeue());
+        System.out.println(q);
+        System.out.println(q.peek());
+    }
+}
