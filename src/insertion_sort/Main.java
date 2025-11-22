6d660cde6ed5bd704ca761b8456b508d15975a63
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-11-22 19:04:42 +0530

    insertion sort

--- src/insertion_sort/Main.java
+++ src/insertion_sort/Main.java
@@ -3,5 +3,15 @@ package insertion_sort;
 public class Main {
     public static void main(String[] args) {
 
+        int[] numbers = {3,4,5,6,2,8};
+        InsertionSort is = new  InsertionSort();
+
+        System.out.println("Before sorting: ");
+        is.printArray(numbers);
+
+        is.insertionSort(numbers);
+
+        System.out.println("After sorting: ");
+        is.printArray(numbers);
     }
 }
